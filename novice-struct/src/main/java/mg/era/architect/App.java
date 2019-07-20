c4b255a441a2429d7f69875441c4aae0f0ad0b45
Elie RASOANAIVO <el.rasoanaivo@gmail.com>
2019-07-20 21:06:54 +0200

    feat(mvn): ajout du pom pour créer un projet maven

---
+++ novice-struct/src/main/java/mg/era/architect/App.java
@@ -0,0 +1,13 @@
+package mg.era.architect;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
