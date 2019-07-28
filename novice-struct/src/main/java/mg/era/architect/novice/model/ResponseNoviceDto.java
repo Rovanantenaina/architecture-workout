dc7c26b562f7e1162f109681ddc5bbbf39bb8691
Elie RASOANAIVO <el.rasoanaivo@gmail.com>
2019-07-28 08:42:10 +0200

    feat(swagger): configuration redis:spring-data

--- novice-struct/src/main/java/mg/era/architect/novice/model/ResponseNoviceDto.java
+++ novice-struct/src/main/java/mg/era/architect/novice/model/ResponseNoviceDto.java
@@ -1,11 +1,13 @@
 package mg.era.architect.novice.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @EqualsAndHashCode(callSuper = true)
 @Data
 @NoArgsConstructor
